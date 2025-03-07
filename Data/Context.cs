a80b3d128715414ebd3828782dbf2e14211d3d3d
Jablonski <Jablonski@SB-1491276>
2025-03-07 13:26:13 -0300

    Adicionar arquivos de projeto.

---
+++ Data/Context.cs
@@ -0,0 +1,24 @@
+﻿using ExemploEF.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExemploEF.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+        }
+
+        // Tabelas do Banco de Dados
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+
+        // Configurações das Entidades
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cliente>().ToTable("Clientes");
+            modelBuilder.Entity<Categoria>().ToTable("Categorias");
+        }
+    }
+}
+
